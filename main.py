c55c40457168988aabfe1c7daad80614c33070b6
Mikey <michael.efs123@gmail.com>
2026-05-23 03:18:51 +0800

    created gym.db table

--- main.py
+++ main.py
@@ -13,10 +13,24 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True  # IMPORTANT
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+#setting up sqlite3
+conn = sqlite3.connect("gym.db")
+cursor = conn.cursor()
+
+#initialize table: gym_sessions
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS gym_sessions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    person TEXT,
+    date TEXT,
+    duration_minutes INTEGER
+)
+""")
+conn.commit()
 
 @bot.event
 async def on_ready():
@@ -28,6 +42,11 @@ async def gymmed(ctx):
     username = ctx.author.name
     streak = None
     await ctx.send(f"{username} has exercised today! Current streak:{streak}")
+
+    # run command
+
+    # checks username
+        #if the username is not in the database yet, create it
 
 
 bot.run(TOKEN)
