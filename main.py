92db6f16e7e0c6d3b7d7524394dba288a9d932b5
Mikey <michael.efs123@gmail.com>
2026-05-23 22:54:44 +0800

    created database setup

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 import sqlite3
+from datetime import date
 
 import discord
 import numpy as np
 import pandas as pd
-from datetime import date
 from discord.ext import commands
 from dotenv import load_dotenv
 from plotly_calplot import calplot
 
+# discord side setup
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -16,23 +17,27 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Change the symbol to your own liking!
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-#setting up sqlite3
-conn = sqlite3.connect("gym.db")
+# Database setup
+conn = sqlite3.connect('gym.db')
 cursor = conn.cursor()
 
-#initialize table: gym_sessions
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS gym_sessions (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    person TEXT NOT NULL,
-    date TEXT NOT NULL,
-    duration_minutes INTEGER NOT NULL
-)
+    CREATE TABLE IF NOT EXISTS gym_sessions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL
+        person TEXT NOT NULL,
+        date TEXT NOT NULL,
+        duration INTEGER NOT NULL
+    )
 """)
+
 conn.commit()
 
+
+# codinggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg :)
 @bot.event
 async def on_ready():
     print("jimbot online now")
@@ -41,28 +46,24 @@ async def on_ready():
 @bot.command()
 async def gymmed(ctx, duration: int):
 
-    username = ctx.author.name
+    user_id = ctx.author.id
     today = date.today().isoformat()
-
     streak = 0
-
-    # connect to database
 
     # check if username exists
 
     # if user does not exist:
-        # create user entry
+    # create user entry
 
     # insert today's gym session (duration)
 
     # calculate streak
 
+    await ctx.send(
+        f" {today} {ctx.author.name} has exercised for {duration} minutes. Current daily streak: {streak}"
+    )
+
     # generate heatmap
 
-    await ctx.send(f" {today} {username} has exercised for {duration} minutes. Current daily streak: {streak}")
+
 bot.run(TOKEN)
-
-
-
-
-
